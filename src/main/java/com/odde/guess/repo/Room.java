833f81d8afb462be7893216e54682c5d41cdc59a
Wu <vic.wu@sap.com>
2018-10-14 15:31:02 +0800

    :recycle: move code to room

--- src/main/java/com/odde/guess/repo/Room.java
+++ src/main/java/com/odde/guess/repo/Room.java
@@ -1,11 +1,14 @@
 package com.odde.guess.repo;
 
+import com.odde.guess.service.VerifyService;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
 
 @Data
 @Entity
@@ -22,12 +25,24 @@ public class Room {
         this.secret = secret;
     }
 
-    public boolean equalDigital(String guess, int i) {
+    private boolean equalDigital(String guess, int i) {
         return guess.charAt(i) == secret.charAt(i);
     }
 
-    public boolean containDigital(String guess, int i) {
+    private boolean containDigital(String guess, int i) {
         return secret.indexOf(guess.charAt(i)) >= 0;
-
     }
+
+    public String verify(String guess) {
+        long countA = count(guess.length(), i -> equalDigital(guess, i));
+
+        long countB = count(guess.length(), i -> containDigital(guess, i)) - countA;
+
+        return String.format("%dA%dB", countA, countB);
+    }
+
+    private long count(int length, IntPredicate predicate) {
+        return IntStream.range(0, length).filter(predicate).count();
+    }
+
 }
--- src/main/java/com/odde/guess/service/VerifyService.java
+++ src/main/java/com/odde/guess/service/VerifyService.java
@@ -1,12 +1,8 @@
 package com.odde.guess.service;
 
-import com.odde.guess.repo.Room;
 import com.odde.guess.repo.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.function.IntPredicate;
-import java.util.stream.IntStream;
 
 @Service
 public class VerifyService {
@@ -18,18 +14,7 @@ public class VerifyService {
     }
 
     public String verify(long id, String guess) {
-        Room room = repo.findById(id);
-
-        long countA = count(guess.length(), i -> room.equalDigital(guess, i));
-
-        long countB = count(guess.length(), i -> room.containDigital(guess, i)) - countA;
-
-
-        return String.format("%dA%dB", countA, countB);
-    }
-
-    private long count(int length, IntPredicate predicate) {
-        return IntStream.range(0, length).filter(predicate).count();
+        return repo.findById(id).verify(guess);
     }
 
 }
