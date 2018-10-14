f4a7a9b83b740320916d4a124cb82d22dcf8b8b4
Wu <vic.wu@sap.com>
2018-10-14 14:56:21 +0800

    deploy done

--- src/main/java/com/odde/guess/GuessApplication.java
+++ src/main/java/com/odde/guess/GuessApplication.java
@@ -2,9 +2,10 @@ package com.odde.guess;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GuessApplication {
+public class GuessApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(GuessApplication.class, args);
